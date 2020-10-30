58ddf55c08ce2fde43985b3a46663933450cf35d
unknown <diptobiswas1998@gmail.com>
2020-10-30 23:34:54 +0530

    [Dipto] refactor: Code added to load Census info of the Indian States from a csv file

---
+++ CensusAnalyserLive/CensusAnalyserLive/CensusAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace CensusAnalyserLive.POCO
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,
+        }
+        Dictionary<string, CensusDTO> dataMap;
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+    }
+}
